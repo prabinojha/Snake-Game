53f187b5aaf67a48cdaa4baaa93983fe681e50d4
Prabin Ojha <prabin.ojha@education.nsw.gov.au>
2023-06-28 12:04:53 +1000

    minor changes

--- main.py
+++ main.py
@@ -16,7 +16,21 @@ class Snake:
     pass
 
 class Food:
-    pass
+    
+    # This is the constructor for the food class
+    
+    def __init__(self):
+        
+        # x position of the food is randomly generated between 0 and the width of the game
+        x = random.randint(0, (GAME_WIDTH/SPACE_SIZE)-1) * SPACE_SIZE
+        
+        # y position of the food is randomly generated between 0 and the height of the game
+        y = random.randint(0, (GAME_HEIGHT/SPACE_SIZE)-1) * SPACE_SIZE
+        
+        self.coordinates = [x, y]
+        
+        # Creating the food on the canvas 
+        canvas.create_oval(x, y, x+SPACE_SIZE, y+SPACE_SIZE, fill=FOOD_COLOUR, tag="food")
 
 def next_turn():
     pass
@@ -38,7 +52,28 @@ score = 0
 direction = 'down'
 
 label = Label(window, text=("Score:{}".format(score)))
+label.pack()
 
+canvas = Canvas(window, bg=BACKGROUND_COLOUR, height=GAME_HEIGHT, width=GAME_WIDTH)
+canvas.pack()
 
+# Centering the window on the screen when it appears upon starting
+
+window.update()
+
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int((screen_width/2) - (window_width/2))
+y = int((screen_height/2) - (window_height/2))
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+# Setting the snake and class
+
+snake = Snake()
+food = Food()
 
 window.mainloop()
